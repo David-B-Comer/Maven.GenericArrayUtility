49ef2c5a0ca850c8da1b84fc1f5aef044d4b0d4f
David Comer <50469980+David-B-Comer@users.noreply.github.com>
2020-03-22 15:35:12 -0400

    done

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,72 @@
 package com.zipcodewilmington.arrayutility;
 
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+
+    private T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+
+    }
+
+    public Integer countNumberOfOccurrences(T[] array, T valueToEvaluate) {
+        int numberOfDuplicates = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(valueToEvaluate)) {
+                numberOfDuplicates++;
+            }
+        }
+        return numberOfDuplicates;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+      return countNumberOfOccurrences(array, valueToEvaluate);
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+       return countNumberOfOccurrences(ArrayUtils.addAll(array, arrayToMerge), valueToEvaluate);
+
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+
+        HashMap<T, Integer> merged = new HashMap<>();
+
+        T[] both = ArrayUtils.addAll(array, arrayToMerge);
+
+        for (int i = 0; i < both.length; i++) {
+            if (merged.containsKey(both[i])) {
+                int count = merged.get(both[i]);
+                count++;
+                merged.put(both[i], count);
+            }
+            else {
+                merged.put(both[i], 1);
+            }
+        }
+        T mostCommon = null;
+        int currentHighest = 0;
+
+        for (Map.Entry<T, Integer> entry : merged.entrySet()) {
+            if (entry.getValue() >= currentHighest) {
+                mostCommon = entry.getKey();
+                currentHighest = entry.getValue();
+            }
+        }
+
+    return mostCommon;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+    return ArrayUtils.removeAllOccurences(array, valueToRemove);
+    }
 }
